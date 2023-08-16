92cd56edc0a73dc4d1bbaa49d8befe7baae88507
reynaldomarchell <reynaldomarchellba@gmail.com>
2023-08-17 01:55:02 +0700

    repair some code

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -7,7 +7,9 @@ export default function Stats({ items }) {
     );
 
   const numItems = items.length;
+
   const numPacked = items.filter((item) => item.packed).length;
+
   const percentage = Math.round((numPacked / numItems) * 100);
 
   return (
